f8ea97268ae7327c293c87b040745e66badb70f9
turalgasimov <turallgasimov@gmail.com>
2026-06-30 20:25:18 -0400

    updated Percolation and PercolationStats based on grader checkstyle for imports

--- union-find/Percolation.java
+++ union-find/Percolation.java
@@ -1,4 +1,7 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
--- union-find/PercolationStats.java
+++ union-find/PercolationStats.java
@@ -1,4 +1,7 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 
 
 public class PercolationStats {
