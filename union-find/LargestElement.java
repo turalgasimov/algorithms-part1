bc94c985077a43cd57abee442ea6f64f53d2f320
turalgasimov <turallgasimov@gmail.com>
2026-07-06 16:26:34 -0400

    refactored files

--- union-find/LargestElement.java
+++ union-find/LargestElement.java
@@ -1,21 +1,21 @@
 import edu.princeton.cs.algs4.*;
 
-
 public class LargestElement {
 
-    static int[] list = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    static int[] id = {7, 8, 8, 5, 5, 5, 8, 7, 8};
+    static int[] list = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+    static int[] id = { 7, 8, 8, 5, 5, 5, 8, 7, 8 };
 
     static int id1 = 8;
     static int id2 = 5;
     static int id3 = 7;
 
-    static int[] comp1 = {1, 2, 6, 8};
-    static int[] comp2 = {3, 4, 5};
-    static int[] comp3 = {0, 7};
+    static int[] comp1 = { 1, 2, 6, 8 };
+    static int[] comp2 = { 3, 4, 5 };
+    static int[] comp3 = { 0, 7 };
 
     /**
      * Returns the largest element in the connected component
+     * 
      * @param i the element that is a part of the connected component
      * @return the largest element in the connected component
      */
@@ -33,5 +33,5 @@ public class LargestElement {
         int i = StdIn.readInt();
         System.out.println(find(i));
     }
-    
+
 }
